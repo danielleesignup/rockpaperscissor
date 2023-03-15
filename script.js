5aa77f5ca812645e736d0b7c35b67598ac8d97a3
Daniel Lee <danielleesignup@gmail.com>
2023-03-16 07:10:12 +0900

    Set program to reset after five rounds
    One thing I hope to learn in the future is how to compartmentalize
    functions so that I don't cram all my functions under one
    function like how I did on this one. I think this is due to my lack of
    understanding in DOM manipulation. Also, I hope to understand someday
    how to make it so that my alert comes after the fifth round is displayed
    on screen. The result of the fifth round is displayed after the alert.
    The player won't know the result of the fifth round until they've seen
    the result of the whole game first. But I still am pround of myself for
    being able to make this simple game using only javascript and html
    regardless of how un-user friendly it is.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 var compAnswer ="";
 var userAnswer ="";
+var rounds = 0;
+var userScore = 0;
+var compScore = 0;
 
 function computerAnswer() {
 	random = Math.floor(Math.random()*3+1)
@@ -18,18 +21,81 @@ function computerAnswer() {
 	
 }
 
+function isItOver() {
+	if (rounds === 5) {
+		if (userScore > compScore) {
+			alert("User Wins!")
+		}
+		else if (userScore < compScore) {
+			alert("Computer Wins!")
+		}
+		else if (userScore == compScore) {
+			alert("It's a tie!")
+		}
+		compScore = 0;
+		userScore = 0;
+		rounds = 0;
+	}
+}
+
+
+function getUserScore() {
+	userScore += 1;
+	rounds += 1;
+	// if (rounds === 5) {
+	// 	if (userScore > compScore) {
+	// 		alert("User Wins!")
+	// 	}
+	// 	else if (userScore < compScore) {
+	// 		alert("Computer Wins!")
+	// 	}
+	// 	else if (userScore == compScore) {
+	// 		alert("It's a tie!")
+	// 	}
+	// 	compScore = 0;
+	// 	userScore = 0;
+	// 	rounds = 0;
+	// }
+}
+
+function getCompScore() {
+	compScore += 1;
+	rounds += 1;
+	// if (rounds === 5) {
+	// 	if (userScore > compScore) {
+	// 		alert("User Wins!")
+	// 	}
+	// 	else if (userScore < compScore) {
+	// 		alert("Computer Wins!")
+	// 	}
+	// 	else if (userScore == compScore) {
+	// 		alert("It's a tie!")
+	// 	}
+	// 	compScore = 0;
+	// 	userScore = 0;
+	// 	rounds = 0;
+	// }
+}
+
+
 function winnerIsFunction() {
 	if (compAnswer === userAnswer) {
+		rounds += 1
 		return 'tie'
 	}
 	else if (
 		compAnswer == 'rock' && userAnswer == 'scissor' ||
 		compAnswer == 'paper' && userAnswer == 'rock' ||
 		compAnswer == 'scissor' && userAnswer == 'paper'
-	)
+	) {
+		getCompScore()
 		return 'Computer'
-	else 
+	}
+	else {
+		getUserScore()
 		return 'User'
+	}
+
 }
 
 
@@ -39,9 +105,13 @@ var rock = document.getElementById('rock').addEventListener('click', function() 
 	winnerIs = winnerIsFunction();
 	document.getElementById("youSaid").innerHTML = '<h3>'+'You said: '+'rock'+'</h3>';
 	document.getElementById("compSaid").innerHTML = '<h3>'+'Computer said: '+ compAnswer+'</h3>';
-	document.getElementById("theWinner").innerHTML = '<h3>'+'The Winner is: '+ winnerIs +'</h3>';
+	document.getElementById("theWinner").innerHTML = '<h3>'+'This Round Winner is: '+ winnerIs +'</h3>';
 	console.log('userAnswer: ', userAnswer)
 	console.log('compAnswer: ', compAnswer)
+	document.getElementById("rounds").innerHTML = '<h3>'+'Rounds: '+ rounds +'</h3>';
+	document.getElementById("userScore").innerHTML = '<h3>'+'User Score: '+userScore+'</h3>';
+	document.getElementById("compScore").innerHTML = '<h3>'+'Computer Score: '+compScore+'</h3>';
+	isItOver();
 }	
 )
 	
@@ -53,25 +123,38 @@ var paper = document.getElementById('paper').addEventListener('click', function(
 	winnerIs = winnerIsFunction();
 	document.getElementById("youSaid").innerHTML = '<h3>'+'You said: '+'paper'+'</h3>';
 	document.getElementById("compSaid").innerHTML = '<h3>'+'Computer said: '+ compAnswer+'</h3>';
-	document.getElementById("theWinner").innerHTML = '<h3>'+'The Winner is: '+ winnerIs +'</h3>';
+	document.getElementById("theWinner").innerHTML = '<h3>'+'This Round Winner is: '+ winnerIs +'</h3>';
 	console.log('userAnswer: ', userAnswer)
 	console.log('compAnswer: ', compAnswer)
+	document.getElementById("rounds").innerHTML = '<h3>'+'Rounds: '+ rounds +'</h3>';
+	document.getElementById("userScore").innerHTML = '<h3>'+'User Score: '+userScore+'</h3>';
+	document.getElementById("compScore").innerHTML = '<h3>'+'Computer Score: '+compScore+'</h3>';
+	isItOver();
 })
 
 var paper = document.getElementById('scissor').addEventListener('click', function(){
 	compAnswer = computerAnswer()
 	userAnswer = 'scissor';
 	winnerIs = winnerIsFunction();
-	document.getElementById("youSaid").innerHTML = '<h3>'+'You said: '+'scissor'+'</h3>';
+	document.getElementById("youSaid").innerHTML = '<h3>'+'You said: '+ userAnswer +'</h3>';
 	document.getElementById("compSaid").innerHTML = '<h3>'+'Computer said: '+ compAnswer+'</h3>';
-	document.getElementById("theWinner").innerHTML = '<h3>'+'The Winner is: '+ winnerIs +'</h3>';
+	document.getElementById("theWinner").innerHTML = '<h3>'+'This Round Winner is: '+ winnerIs +'</h3>';
 	console.log('userAnswer: ', userAnswer)
 	console.log('compAnswer: ', compAnswer)
+	document.getElementById("rounds").innerHTML = '<h3>'+'Rounds: '+ rounds +'</h3>';
+	document.getElementById("userScore").innerHTML = '<h3>'+'User Score: '+userScore+'</h3>';
+	document.getElementById("compScore").innerHTML = '<h3>'+'Computer Score: '+compScore+'</h3>';
+	isItOver();
 })
 
 
+document.getElementById("youSaid").innerHTML = '<h3>'+'You said: '+ userAnswer +'</h3>';
+document.getElementById("compSaid").innerHTML = '<h3>'+'Computer said: '+ compAnswer+'</h3>';
+document.getElementById("theWinner").innerHTML = '<h3>'+'The Winner is: '+ winnerIs +'</h3>';
+console.log('userAnswer: ', userAnswer)
+console.log('compAnswer: ', compAnswer)
 
 
-// const para = document.createElement('p');
-// para.textContent = ;
-// output.appendChild(para);
+
+
+
